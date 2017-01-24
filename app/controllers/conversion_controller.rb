42b19fddbe6ca959358344a75bcba5e90d8c661d
ShaileshPrajapati-BTC <prajapatishailesh@botreetechnologies.com>
2017-01-24 17:17:34 +0530

    -- moved code to conversion controller

---
+++ app/controllers/conversion_controller.rb
@@ -0,0 +1,19 @@
+class ConversionController < ApplicationController
+  def index
+    redirected_api_url = "https://blockchain.info/ticker"
+    @Currency = JSON.parse(Net::HTTP.get(URI.parse(redirected_api_url)))
+  end
+
+  def to_bitcoin
+    uri = URI.parse("https://blockchain.info/tobtc?currency=#{params[:currency]}&value=#{params[:value]}")
+    @response = Net::HTTP.get(uri)
+    render :json => @response
+  end
+
+  def bit_to_currency
+    uri = URI.parse("https://bitpay.com/api/rates")
+    @response = Net::HTTP.get(uri)
+    rate = JSON.parse(@response).select {|currency| params[:currency] == currency["code"]}
+    render :json =>  (rate[0]["rate"] * params[:value].to_i).to_s,  status: 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
-  post "/to_bitcoin" => "visitors#to_bitcoin"
-  post "/bit_to_currency" => "visitors#bit_to_currency"
+  root to: 'conversion#index'
+  post "/to_bitcoin" => "conversion#to_bitcoin"
+  post "/bit_to_currency" => "conversion#bit_to_currency"
 end

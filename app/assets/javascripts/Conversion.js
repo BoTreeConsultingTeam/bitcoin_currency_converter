27ebe72d6f384c7136887784ccd18fb3a6ea65b9
ShaileshPrajapati-BTC <prajapatishailesh@botreetechnologies.com>
2017-01-25 17:46:22 +0530

    -- added instance method and strong parameters.

--- app/assets/javascripts/Conversion.js
+++ app/assets/javascripts/Conversion.js
@@ -4,7 +4,7 @@ Currency.Convert = {
   toBitcoin: function(){
     $('#convert').on("click",function(){
       if($('#value').val().length>0){
-        var p =  $.post( "/currency_to_bitcoin", { currency: $('#Currency').val(),value: $('#value').val() } );
+        var p =  $.post( "/currency_to_bitcoin", {conversion: { currency: $('#Currency').val(),value: $('#value').val() }} );
         p.done(function( data ) {
           $( "#result" ).empty().append("Bitcoin " + data );
         })
@@ -17,7 +17,7 @@ Currency.Convert = {
   toCurrency: function(){
     $('#bittocurrency').on("click",function(){
       if($('#bitcoin_value').val().length>0){
-        var p =  $.post( "/bitcoin_to_currency", { currency: $('#BCurrency').val(),value: $('#bitcoin_value').val()});
+        var p =  $.post( "/bitcoin_to_currency", {conversion: { currency: $('#BCurrency').val(),value: $('#bitcoin_value').val()}});
         p.done(function( data ) {
           $( "#bit_to_curr" ).empty().append($('#BCurrency').val()+" "+ data );
         })
--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -1,9 +1,19 @@
 class ConversionController < ApplicationController
   def currency_to_bitcoin
-    render :json => CurrencyConversionService.new(params).currency_to_bitcoin
+    render :json => conversion_service.currency_to_bitcoin
   end
 
   def bitcoin_to_currency
-    render :json =>  CurrencyConversionService.new(params).bitcoin_to_currency
+    render :json =>  conversion_service.bitcoin_to_currency
+  end
+
+  private
+
+  def conversion_service
+    CurrencyConversionService.new(conversion_params)
+  end
+
+  def conversion_params
+    params.require(:conversion).permit(:currency, :value)
   end
 end
